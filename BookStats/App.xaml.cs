171a0b613660dcb129e8b1cc4688d06b16bf3bfe
ReckyCless <dimaandkudr@gmail.com>
2023-12-04 20:12:55 +0300

    Добавьте файлы проекта.

---
+++ BookStats/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using BookStats.Models;
+
+namespace BookStats
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static BooksStatsDBEntities Context { get; } = new BooksStatsDBEntities();
+        public static Users CurrentUser = null;
+    }
+}
---
+++ BookStats/Pages/ToolPages/AuthPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BookStats.Windows;
+
+namespace BookStats.Pages.ToolPages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var currentUser = App.Context.Users.Where(p => (p.Login == tbLogin.Text || p.Email == tbLogin.Text) && p.Password == tbPassword.Password)
+                .AsEnumerable()
+                .FirstOrDefault(p => (p.Login == tbLogin.Text || p.Email == tbLogin.Text) && p.Password == tbPassword.Password);
+            if (currentUser != null)
+            {
+                App.CurrentUser = currentUser;
+                var currentWindow = Application.Current.Windows
+                    .Cast<Window>()
+                    .FirstOrDefault(window => window is MainWindow) as MainWindow;
+
+                MessageBox.Show("Прошло удачно!");
+                /* NavigationService.Navigate(new Pages.ProductView());*/
+
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            App.CurrentUser = null;
+            tbLogin.Text = String.Empty;
+            var currentWindow = Application.Current.Windows
+                .Cast<Window>()
+                .FirstOrDefault(window => window is MainWindow) as MainWindow;
+            /*            currentWindow.AdminSidePanel.Visibility = Visibility.Collapsed;
+                        currentWindow.UserSidePanel.Visibility = Visibility.Collapsed;
+                        currentWindow.CabinetMenu.Visibility = Visibility.Collapsed;*/
+        }
+
+        private void BtnToRegister_Click(object sender, RoutedEventArgs e)
+        {
+            /*NavigationService.Navigate(new RegisterPage(null));*/
+        }
+    }
+}
